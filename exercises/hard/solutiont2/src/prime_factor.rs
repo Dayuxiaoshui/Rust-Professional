13c07fddc37fca04030e1df407fa6077733a84ea
大雨小水 <792179245@qq.com>
2025-02-07 17:06:22 +0800

    third

--- exercises/hard/solutiont2/src/prime_factor.rs
+++ exercises/hard/solutiont2/src/prime_factor.rs
@@ -1,3 +1,25 @@
 pub fn find_max_prime_factor(number: u128) -> u128 {
-    todo!()
+    let mut num = number;
+    let mut max_prime = 1;
+
+    // 处理2，直到不能整除为止
+    while num % 2 == 0 {
+        max_prime = 2;
+        num /= 2;
+    }
+
+    let mut factor = 3;
+    while factor * factor <= num {
+        while num % factor == 0 {
+            max_prime = factor;
+            num /= factor;
+        }
+        factor += 2;  
+    }
+
+    if num > 1 {
+        max_prime = num;
+    }
+
+    max_prime
 }
